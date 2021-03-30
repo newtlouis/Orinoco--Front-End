2f62d32f92b644b63ec7c7efa4375d17dafda2be
Louis <louis.m.newton@hotmail.fr>
2021-03-30 11:07:46 +0200

    Envoie du formulaire et de la commande au serveur + possibilité d'envoyer plusieurs commandes

--- panier.js
+++ panier.js
@@ -1,3 +1,4 @@
+// AFFICHAGE DES PRODUIS DANS LE PANIER
 // Selection de la balise dans laquelle on va inserer les data du produits choisi
 const positionElement = document.querySelector(".container__panier");
 
@@ -12,12 +13,6 @@ if (panier){
 // Bouche for pour ajouter les produits du panier dans la page panier
 structurePanier = "";
 panier.forEach((element, i) => {
-    // id[i] =  element._id;
-    // nom[i] = element.name;
-    // price[i] = element.price;
-    // description[i] = element.description;
-    // imageUrl[i] = element.imageUrl;
-    // colors[i] = element.colors;
 
     // Création des textes qu'on va injecter dans le document
     structurePanier += 
@@ -48,7 +43,7 @@ panier.forEach(element => {
     total+= element.prix;
 } );
 
-console.log(total);
+
 
 // Insertion du total dans le panier
 positionTotal = document.querySelector(".total");
@@ -72,7 +67,7 @@ structureFormulaire = `
     <input type="text" id="ville" name="ville" required>
 
     <label for="codePostal">Code postal:</label>
-    <input type="text" id="codePostal name="codePostal" required>
+    <input type="text" id="codePostal" name="codePostal" required>
 
     <label for="email">Email:</label>
     <input type="text" id="email" name="email" required>
@@ -90,3 +85,63 @@ positionFormulaire.innerHTML = structureFormulaire;
 else {
     positionElement.innerHTML = "Votre panier est vide"
 }
+
+
+// ENVOIE DU FORMULAIRE AU LOCAL STORAGE
+
+// Au clic du bouton envoyer
+btnEnvoyerFormulaire = document.querySelector("#envoyerFormulaire");
+btnEnvoyerFormulaire.addEventListener("click",(e) => {
+    e.preventDefault();
+
+  
+    // Mettre les données du formulaire dans un objet
+    formulaire = {
+        prenom : document.querySelector("#prenom").value,
+        nom : document.querySelector("#nom").value,
+        adresse : document.querySelector("#adresse").value,
+        ville : document.querySelector("#ville").value,
+        codePostal : document.querySelector("#codePostal").value,
+        email : document.querySelector("#email").value,
+    };
+
+    // Création de l'objet qu'il faut envoyer au serveur
+    infoPourLeServeur = {
+        formulaire,
+        panier,
+    };
+
+    // // Envoie du formulaire de contact, ainsi que les produits commandés au serveur
+    // localStorage.setItem("Commandes",JSON.stringify(infoPourLeServeur));
+    // console.log(infoPourLeServeur);
+
+     // Connexion au local storage et récupération du panier
+     let commandes = JSON.parse(localStorage.getItem("Commandes"));
+     console.log(commandes);
+ 
+     // Si les commandes enregistrées n'est pas vide: alors on y ajoute le nouveau produit et on renvoit le tout 
+     if(commandes) {
+         commandes.push(infoPourLeServeur);
+         localStorage.setItem("Commandes",JSON.stringify(commandes));
+        //  popUpConfirmation();
+     }
+     // S'il est vide, alors on crée un panier
+     else {
+         commandes = [];
+         commandes.push(infoPourLeServeur);
+         localStorage.setItem("Commandes",JSON.stringify(commandes));
+        //  popUpConfirmation();
+         
+     }
+ 
+    //    // Fonction pop up de confirmation
+    //    function popUpConfirmation () {
+    //      if ( window.confirm(`${nom} en couleur ${optionChoice} à bien été ajouter au panier. Cliquez sur OK pour consulter votre panier et ANNULER pour revenir à la page d'accueil.`) )
+    //      {
+    //          window.location.href = "panier.html";
+    //      }
+    //      else { window.location.href = "index.html";}
+    //  }
+
+
+})
